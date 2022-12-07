96d701291d9797b39ddc800eeddf8ae234f679ec
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-12-07 03:52:46 -0800

    updated sqlalchemy of class User that inherited from BaseModel and Base

--- models/user.py
+++ models/user.py
@@ -2,13 +2,13 @@
 
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Sequence
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models import storage_type
 from os import getenv
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
      __tablename__ = 'users'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
