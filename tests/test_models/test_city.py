9cbdcceb99528a5b7fe424a742abd3fee996b7da
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-12-07 05:26:57 -0800

    models tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
-""" """
+
+:x
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import os
 
 
 class test_City(test_basemodel):
-    """ """
+    """tests for City"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """initializing City test """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """test for state_id """
         new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        self.assertEqual(type(new.state_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_name(self):
-        """ """
+        """test for name """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
