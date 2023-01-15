fbee26872b9a940a8ef6d3f4be30e5d6ac30c6c6
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-15 03:05:33 -0800

    Script that starts a Flask web application and displays the states and cities objects sorted alphabetically form A->Z

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
 '''
-Script that starts a Flask web application:
-    The web application must be listening on 0.0.0.0, port 5000
-    Use storage for fetching data from the storage engine (FileStorage or
-    DBStorage) => from models import storage and storage.all(...)
-    To load all cities of a State:
-        If the storage engine is DBStorage, use cities relationship
-        Otherwise, use the public getter method cities
-    After each request remove the current SQLAlchemy Session:
-        Declare a method to handle @app.teardown_appcontext
-        Call in this method storage.close()
-    Routes:
-        /cities_by_states: display a HTML page: (inside the tag BODY)
-            H1 tag: “States”
-            UL tag: with the list of all State objects present in
-            DBStorage sorted by name (A->Z) tip
-                LI tag: description of one State: <state.id>:
-                <B><state.name></B> + UL tag: with the list of
-                City objects linked to the State sorted by name (A->Z)
-                    LI tag: description of one City: <city.id>:
-                    <B><city.name></B>
-    Use the option strict_slashes=False in the route definition
+Script that starts a Flask web application and displays the states
+and cities objects sorted alphabetically form A->Z
 '''
 from models import storage
 from models import *
