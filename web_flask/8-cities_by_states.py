748011a8935a0b38637b160079a528050ae2bc21
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-15 10:19:36 -0800

    Script that starts a Flask web application and displays the states and cities objects sorted alphabetically form A->Z

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,22 +4,23 @@ Script that starts a Flask web application and displays the states
 and cities objects sorted alphabetically form A->Z
 '''
 from models import storage
-from models import *
+from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    '''Returns the states and cities list sorted in A->Z'''
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
-
-
 @app.teardown_appcontext
 def close_database(err):
     '''Closes all the sqlalchemy of the database'''
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    '''Returns the states and cities list sorted in A->Z'''
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
