8e7d57983c738287d96d64368f54641090aad191
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-15 10:29:27 -0800

    Script that starts a Flask web appliction and lists all the states and cities objects alphabeticallly in A->Z. This also includes the HTML pages

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,12 +5,13 @@ cities objects alphabeticallly in A->Z. This also includes the HTML pages
 '''
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_database(error):
+def close_database(self):
     '''Closes all sqlalchemy session in the database'''
     storage.close()
 
@@ -18,15 +19,17 @@ def close_database(error):
 @app.route('/states/', strict_slashes=False)
 def states():
     '''Returns the list of all states'''
-    state = storage.all('State')
-    return render_template('9-states.html', state=state)
+    state = storage.all(State)
+    return render_template('9-states.html', state=state, mode='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
     '''Returns the list all the states by id'''
-    state = storage.all('State')
-    return render_template('9-states.html', state=state, id=id)
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
 
 
 if __name__ == "__main__":
